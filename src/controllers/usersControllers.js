2421429942c392448c446a89cc568183c72dbaed
Marie-Sail <marie.saillard.pro@gmail.com>
2023-11-29 16:51:32 +0100

    challenge 4

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,10 +1,9 @@
 const database = require("../../database");
 
-
 const getUsers = (req, res) => {
   database
-  .query("select * from users")
-  .then(([users]) => {
+    .query("select * from users")
+    .then(([users]) => {
       if (users) {
         res.json(users);
         res.status(200);
@@ -34,13 +33,12 @@ const getUsersById = (req, res) => {
       console.error(err);
       res.sendStatus(500);
     });
-  };
-  
-  const postUsers = (req, res)=> {
-  
-    const {firstname, lastname, email, city, language} = req.body;
-  
-    database
+};
+
+const postUsers = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
     .query(
       "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?,?,?,?,?)",
       [firstname, lastname, email, city, language]
@@ -52,17 +50,34 @@ const getUsersById = (req, res) => {
       console.error(err);
       res.sendStatus(500);
     });
-  
-  }
-  const updateUser =(req, res) => {
-    const id = parseInt(req.params.id)
-    const {firstname, lastname, email, city, language} = req.body;
+};
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
 
-    database
+  database
     .query(
       " UPDATE users set firstname = ?, lastname = ?, email = ?, city = ?, language = ?  WHERE id = ? ",
       [firstname, lastname, email, city, language, id]
     )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
     .then(([result]) => {
       if (result.affectedRows === 0) {
         res.sendStatus(404);
@@ -81,4 +96,5 @@ module.exports = {
   getUsersById,
   postUsers,
   updateUser,
+  deleteUser,
 };
--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -4,15 +4,14 @@ const userSchema = Joi.object({
   email: Joi.string().email().max(255).required(),
   firstname: Joi.string().max(255).required(),
   lastname: Joi.string().max(255).required(),
-  city:Joi.string().max(255).required(),
-  language: Joi.string().max(255).required(),
+  
 });
 
 const validateUser = (req, res, next) => {
-  const { firstname, lastname, email, city, language } = req.body;
+  const { firstname, lastname, email} = req.body;
 
   const { error } = userSchema.validate(
-    { firstname, lastname, email,  city, language },
+    { firstname, lastname, email},
     { abortEarly: false }
   );
 
