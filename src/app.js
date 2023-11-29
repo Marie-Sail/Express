14f1db3fae5879c4549ea468a62de051bc9f85fb
Marie-Sail <marie.saillard.pro@gmail.com>
2023-11-29 16:32:44 +0100

    challenge 3.2

--- src/app.js
+++ src/app.js
@@ -5,21 +5,25 @@ const app = express();
 app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
+const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUser");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.post("/api/movies",validateMovie, movieControllers.postMovie)
+app.put("/api/movies/:id",validateMovie, movieControllers.putMovies)
 
-app.post("/api/movies", movieControllers.postMovie)
-
-app.put("/api/movies/:id",movieControllers.putMovies)
 
 const usersControllers = require("./controllers/usersControllers");
 
 app.get("/api/users", usersControllers.getUsers);
 app.get("/api/users/:id", usersControllers.getUsersById)
+app.post("/api/users",validateUser, usersControllers.postUsers)
+app.put("/api/users/:id", validateUser, usersControllers.updateUser)
 
-app.post("/api/users", usersControllers.postUsers)
 
-app.put("/api/users/:id",usersControllers.updateUser)
+
+
+
 
 module.exports = app;
